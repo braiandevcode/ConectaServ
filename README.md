1e6d1014bb618fc5ca92f077981fa9fb02349bd2
Braian Palacios <braianpalacios699@gmail.com>
2025-09-08 20:27:57 -0300

    Added environment in configuration database

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # ConectaServ
 
 [Drive ConectaServ](https://drive.google.com/drive/u/1/folders/1ZAfBl2fj-0ov2GnPDK3NFrIPtFejqX0j)
-
-
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,19 +4,40 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { LocationsModule } from './location/locations.module';
 import { CategoryModule } from './category/category.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Tasker } from './tasker/entities/tasker.entity';
+import { Location } from './location/entities/location.entity';
+import { Category } from './category/entities/category.entity';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'admin',
-      database: 'conectaserv1',
-      entities: [],
-      synchronize: true,
-}), LocationsModule, CategoryModule,
-],
+  imports: [
+    // CONFIGURACION DE MODULO PARA VARIABLE DE ENTORNOS
+    ConfigModule.forRoot({
+      // CARGA VARIABLES DE ENTORNO DESDE ARCHIVO .ENV
+      isGlobal: true, // DISPONIBLE EN TODA LA APP SIN VOLVER A IMPORTAR
+      envFilePath: `.env.${process.env.NODE_ENV || 'development'}.local`, // ARCHIVO .ENV SEGUN EL ENTORNO
+    }),
+    // CONECTAR BASE DE DATOS
+    TypeOrmModule.forRootAsync({
+      // CONFIGURA TYPEORM DE FORMA ASÍNCRONA (PERMITE USAR CONFIGSERVICE)
+      imports: [ConfigModule], // IMPORTA CONFIGMODULE PARA PODER USAR VARIABLES DE ENTORNO
+      inject: [ConfigService], // INYECTA CONFIGSERVICE DENTRO DEL useFactory
+      useFactory: (config: ConfigService) => ({
+        // FUNCIÓN QUE DEVUELVE LA CONFIG DE LA DB
+        type: 'mysql', // TIPO DE BASE DE DATOS
+        host: config.get<string>('DB_HOST'), // HOST DE LA DB
+        port: config.get<number>('DB_PORT'), // PUERTO DE LA DB
+        username: config.get<string>('DB_USERNAME'), // USUARIO DE LA DB
+        password: config.get<string>('DB_PASSWORD'), // PASSWORD DE LA DB
+        database: config.get<string>('DB_NAME'), // NOMBRE DE LA BASE DE DATOS
+        entities: [__dirname + '/**/*.entity{.ts,.js}'], // ENTIDADES QUE VA A LEER
+        synchronize: true, // AUTO SINCRONIZA SCHEMA (NO USAR EN PRODUCCIÓN)
+      }),
+    }),
+    Tasker,
+    Location,
+    Category
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
